a05bc53aace2bf6b09173337782effbe340960d6
Aashi Gupta <aashi.ka.backupp@gmail.com>
2024-08-25 00:43:06 +0530

    Update [..sign-up page.js

--- [..sign-up page.js
+++ [..sign-up page.js
@@ -1,1 +1,36 @@
+import {AppBar, Container, Toolbar, Typography, Button} from '@mui/material'
 
+export default function SignUpPage(){
+  return <Container maxwidth = "sm">
+    <AppBar Position = "Static" sx = {{backgroundColor : "#3f51b5"}}
+       <Toolbar>
+         <Typography variant = 'h6' sx = {{
+         flexGrow: 1
+       }}>
+       Flashcard SaaS 
+         </Typography>
+         <Button color = "inherit">
+         <Link href = "/sign_in" passHref>
+         login
+         </Link>
+         </Butoon>
+         <Button color = "inherit">
+         <Link href = "/sign-up" passHref>
+         Sign up
+         </Link>
+         </Butoon>
+         </Toolbar> 
+         </AppBar>
+
+        <Box
+         display = "flex"
+         flexDirection = "column"
+           alignItems = "center"
+             justifyContent = "center">
+               <Typography variant = 'h4'>
+               Sign Up 
+               </Typography>
+               <SignUp />
+         </Box>
+    </Container  
+}
