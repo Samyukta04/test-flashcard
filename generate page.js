a5c866ba0809c734d85436de2432b32e729fa241
sridswetha <sridswetha@gmail.com>
2024-08-25 14:52:29 -0400

    generate page.js (52:29- 1:19:27)

--- generate page.js
+++ generate page.js
@@ -1,8 +1,19 @@
 'use client'
-import {useUser} from '@clerk/nextjs'
-import {useRouter} from 'next/navigation'
+
+import { useUser } from '@clerk/nextjs'
+import { useRouter } from 'next/navigation'
+import {db} from '@/firebase'
+import {doc, collection, setDoc, getDoc, writeBatch} from 'firebase/firestore'
+import {
+  Container,
+  TextField,
+  Button,
+  Typography,
+  Box,
+} from '@mui/material'
+
 export default function Generate() {
-  const {isLoaded, isSignedIn, user} = useUser()
+  const { isLoaded, isSignedIn, user } = useUser()
   const [flashcards, setFlashcards] = useState([])
   const [flipped, setFlipped] = useState([])
   const [text, setText] = useState('')
@@ -10,33 +21,189 @@ export default function Generate() {
   const [open, setOpen] = useState(false)
   const router = useRouter
 
-  const handleSubmit = async ()=>{
-  fetch('api/generate',{ method : 'POST', body: text,})
-    .then((res)=> res.json())
-    .then(data > setFlashcards(data))
+  const handleSubmit = async () => {
+    fetch('api/generate', { method: 'POST', body: text, })
+      .then((res) => res.json())
+      .then(data > setFlashcards(data))
   }
-const handleCardClick = (id) =>{
-setFlipped((prev) => ({
-...prev,
-  [id]: !prev[id],
-}))
-}
-
-  const handleOpen =() => {
-  setOpenI(true)
-  }
-  const handleClose =() => {
-  setOpenI(false)
+  const handleCardClick = (id) => {
+    setFlipped((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }))
   }
 
-  const save Flashcards = async () =>{
-  if(!name){
-  alert('Please enter a name'),
-    return
+  const handleOpen = () => {
+    setOpen(true)
+  }
+  const handleClose = () => {
+    setOpen(false)
   }
 
-    
+  const saveFlashcards = async () => {
+    if (!name) {
+      alert('Please enter a name')
+      return
+    }
+    const batch = writeBatch(db)
+    const userDocRef = doc(collection(db, 'users'), user.id)
+    const docSnap = await getDoc(userDocRef)
+
+    if (docSnap.exists()) {
+      const collections = docSnap.data().flashcards || []
+      if (collections.find((f) => f.name === name)) {
+        alert('Flashcard collection with the same name already exists.')
+        return
+      } else {
+        collections.push({ name })
+        batch.set(userDocRef, { flashcards: collections }, { merge: true })
+      }
+    } else {
+      batch.set(userDocRef, { flaschards: [{ name }] })
+    }
+
+    const colRef = collection(userDocRef, name)
+    flaschards.forEach((flashcard) => {
+      const cardDocRef = doc(colRef)
+      batch.set(cardDocRef, flaschard)
+    })
+    await batch.commit()
+    handleClose()
+    router.push('/flashcards')
   }
 
-  
+  // component itself -> (he said to make the rest of this look good because its quite elementary)
+  return (
+    <Container maxWidth="md">
+      <Box sx={{
+        mt: 4,
+        mb: 6,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}
+      >
+        <Typography variation="h4">Generate Flashcards</Typography>
+        <Paper sx={{ p: 4, width: '100%' }}>
+          <TextField
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            label="Enter text"
+            full width
+            multiline
+            rows={4}
+            variatn="outlined"
+            sx={{
+              mb: 2,
+            }}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleSubmit}
+            fullWidth
+          >
+            {' '}
+            Submit
+          </Button>
+        </Paper>
+      </Box>
+      {flashcards.length > 0 && (
+        <Box sx={{ mt: 4 }}>
+          <Typography variant="h5">Flaschards Preview</Typography>
+          <Grid container spacing={3}>
+            {flaschards.map((flashcard, index) => (
+              <Grid item xs={12} sm={6} md={4} key={index}>
+                <Card>
+                  <CardActionArea
+                    onClick={() => {
+                      handleCardClick(index);
+                    }}
+                  >
+                    <CardContent>
+                      <Box sx={{
+                        persepctive: '1000px',
+                        '& > div': {
+                          transition: 'transfrom 0.6s',
+                          transformStyle: 'preserve-3d',
+                          position: 'relative',
+                          width: '100%',
+                          height: '200px',
+                          boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
+                          transform: flipped[index]
+                            ? 'rotateY(180deg)'
+                            : 'rotateY(0deg)',
+                        },
+
+                        '& > div > div': {
+                          position: 'relative',
+                          width: '100%',
+                          height: '100%',
+                          backfaceVisibility: 'hidden',
+                          display: 'flex',
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                          padding: 2,
+                          boxSizing: 'border-box'
+                        },
+                        '& > div > div:nth-of-type(2)': {
+                          transform: 'rotateY(180deg)'
+                        },
+                      }}
+                      >
+                        <div>
+                          <div>
+                            <Typography variant="h5" component="div">
+                              {flaschard.front}
+                            </Typography>
+                          </div>
+                        </div>
+                        <div>
+                          <div>
+                            <Typography variant="h5" component="div">
+                              {flaschard.back}
+                            </Typography>
+                          </div>
+                        </div>
+                      </Box>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+          <Box sx={{ mt: 4, display: 'flex', justifyContent: 'center' }}>
+            <Button variant="contained" color="secondary" onClick={handleOpen}>
+              Save
+            </Button>
+          </Box>
+        </Box>
+      )}
+
+      <Dialog open={open} onclose={handleClose}>
+        <DialogTitle>Save Flashcards</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please enter name for your flaschards collection
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin='dense'
+            label='collection name'
+            type="text"
+            fullWidth
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            variant="outlined">
+          </TextField>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}> Cancel</Button>
+          <Button onClick={saveFlaschards}> Save </Button>
+
+        </DialogActions>
+      </Dialog>
+
+    </Container >
+  )
 }
